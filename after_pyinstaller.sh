86c24d16b3c521be2b62b78082d833a712faf390
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2021-02-19 18:28:30 -0500

    Add GitHub CI

--- after_pyinstaller.sh
+++ after_pyinstaller.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-mkdir dist/ProperTree.app/Contents/Resources/lib
-cp -R /usr/local/Cellar/tcl-tk/8.6.10/lib/tcl* dist/ProperTree.app/Contents/Resources/lib/
-ln -s Resources/lib dist/ProperTree.app/Contents/
+# mkdir dist/lib
+# cp -R /usr/local/Cellar/tcl-tk/8.6.10/lib/tcl* dist/lib/
+# mkdir dist/ProperTree.app/Contents/Resources/lib
+# cp -Rf /usr/local/Cellar/tcl-tk/8.6.10/lib/tcl* dist/ProperTree.app/Contents/Resources/lib/
+# chmod -R 755 dist/ProperTree.app/Contents/Resources/lib/
+# ln -s Resources/lib dist/ProperTree.app/Contents/
+rm -f dist/ProperTree.app.zip
+cd dist
+zip -r ProperTree.app.zip ProperTree.app
